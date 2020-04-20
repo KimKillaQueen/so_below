cab5540433920d06a48a7e2b6a9f9309ce048225
kimkillaqueen <kimkillaqueen@gmail.com>
2020-04-19 22:54:16 -0800

    added post processing, shovel rotation

--- Assets/Scripts/Interactable_furnace.cs
+++ Assets/Scripts/Interactable_furnace.cs
@@ -6,19 +6,73 @@ public class Interactable_furnace : Interactables
 {
     // Start is called before the first frame update
 
-        private Item item;
+    private float fuelTimer = 50.0f;
+    private float currentFuelLeft = 50.0f;
+    private float refuelTime = 3.0f;
+    private Item item;
+    public List<Light> lights;
+    public List<float> intensities;
+    public bool refueling = false;
 
-        private void Start() {
-            item = new Item();
-            item.name = "coal";
+    private void Start()
+    {
+        item = new Item();
+        item.name = "coal";
+
+        lights = new List<Light>(FindObjectsOfType<Light>());
+        foreach (var light in lights)
+        {
+            intensities.Add(light.intensity);
         }
-        public override void interact(Inventory inventory) { 
-            if(inventory.takeItem(item)) {
-                Debug.Log("Player fed coal to furnace");
-                
-            } else {
-                Debug.Log("Player has no coal");                
+    }
+
+    private void Update()
+    {
+        if (!refueling)
+        {
+            currentFuelLeft -= Time.deltaTime;
+            for (int x = 0; x < lights.Count; x++)
+            {
+                lights[x].intensity = Mathf.Max(intensities[x], lights[x].intensity) * (currentFuelLeft / fuelTimer);
             }
         }
+        // FindObjectsOfType<Light>().intensity *= currentFuelLeft / fuelTimer;
+
+    }
+    public override void interact(Inventory inventory)
+    {
+        if (inventory.takeItem(item))
+        {
+            Debug.Log("Player fed coal to furnace");
+            currentFuelLeft = fuelTimer;
+            refueling = true;
+            StartCoroutine("lightRise");
+        }
+        else
+        {
+            Debug.Log("Player has no coal");
+        }
+    }
+
+    private IEnumerator lightRise()
+    {
+        float timer = 0.0f;
+
+        while (timer < refuelTime)
+        {
+            for (int x = 0; x < lights.Count; x++)
+            {
+                lights[x].intensity += intensities[x] * (timer / refuelTime);
+                lights[x].intensity = Mathf.Min(lights[x].intensity, intensities[x] * 1.25f);
+            }
+            timer += Time.deltaTime;
+
+            yield return null;
+        }
+
+        refueling = false;
+
+        
+    }
 
 }
--- Assets/Scripts/playerInteract.cs
+++ Assets/Scripts/playerInteract.cs
@@ -31,30 +31,33 @@ public class playerInteract : MonoBehaviour
         if(target && Input.GetKeyDown(KeyCode.Space)) {
             target.interact(GetComponent<Inventory>());
             if(!shovelUp && target.GetType() == typeof(Interactable_coalPile)) {
+                shovel.transform.Rotate(Vector3.right, -40.0f);
                 shovelUp = true;
+                
             } else if(target.GetType() == typeof(Interactable_furnace)) {
+                shovel.transform.Rotate(Vector3.right, 40.0f);
                 shovelUp = false;
             }
             
         }
 
-        if(shovelUp && transform.rotation.eulerAngles.x > -90.0f) {
-            // Quaternion tempRotation = Quaternion.AngleAxis(-40.0f, Vector3.right);
-            float degree = Mathf.LerpAngle(transform.rotation.eulerAngles.x, -90.0f, 0.1f);
-            shovel.transform.Rotate(degree, 0.0f, 0.0f);
-        } else if(!shovelUp && transform.rotation.eulerAngles.x < -50.0f) {
-            // shovel.transform.rotation = Quaternion.RotateTowards(shovel.transform.rotation, shovelIntialRotation.rotation, 3.0f);
-            // Vector3 degree = shovelIntialRotation.localToWorldMatrix.rotation.eulerAngles;
-            // shovel.transform.rotation = Quaternion.Euler(degree.x, shovel.transform.rotation.y, shovel.transform.rotation.z);
-            float degree = Mathf.LerpAngle(transform.rotation.eulerAngles.x, -50.0f, 0.1f);
-            shovel.transform.Rotate(-degree, 0.0f, 0.0f);
-            // Quaternion tempRotation = Quaternion.AngleAxis(-40.0f, Vector3.right);
-            // shovel.transform.rotation = Quaternion.Slerp(shovel.transform.rotation, shovelIntialRotation.rotation, 0.1f);
-            // shovelUpRotation = shovelIntialRotation * tempRotation;
-            // shovel.transform.rotation = Quaternion.Slerp(shovel.transform.rotation, shovelIntialRotation, 0.5f);
-        } else {
+        // if(shovelUp && transform.rotation.eulerAngles.x > -90.0f) {
+        //     // Quaternion tempRotation = Quaternion.AngleAxis(-40.0f, Vector3.right);
+        //     float degree = Mathf.LerpAngle(transform.rotation.eulerAngles.x, -90.0f, 0.1f);
+        //     shovel.transform.Rotate(degree, 0.0f, 0.0f);
+        // } else if(!shovelUp && transform.rotation.eulerAngles.x < -50.0f) {
+        //     // shovel.transform.rotation = Quaternion.RotateTowards(shovel.transform.rotation, shovelIntialRotation.rotation, 3.0f);
+        //     // Vector3 degree = shovelIntialRotation.localToWorldMatrix.rotation.eulerAngles;
+        //     // shovel.transform.rotation = Quaternion.Euler(degree.x, shovel.transform.rotation.y, shovel.transform.rotation.z);
+        //     float degree = Mathf.LerpAngle(transform.rotation.eulerAngles.x, -50.0f, 0.1f);
+        //     shovel.transform.Rotate(-degree, 0.0f, 0.0f);
+        //     // Quaternion tempRotation = Quaternion.AngleAxis(-40.0f, Vector3.right);
+        //     // shovel.transform.rotation = Quaternion.Slerp(shovel.transform.rotation, shovelIntialRotation.rotation, 0.1f);
+        //     // shovelUpRotation = shovelIntialRotation * tempRotation;
+        //     // shovel.transform.rotation = Quaternion.Slerp(shovel.transform.rotation, shovelIntialRotation, 0.5f);
+        // } else {
 
-        }
+        // }
 
         updateUI();
     }
